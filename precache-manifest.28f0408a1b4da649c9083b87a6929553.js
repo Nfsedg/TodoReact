cb1d68f59ef7e2a30dd072736063ad877987babc
Edgar Pech <edgaropech@hotmail.com>
2021-10-15 21:20:46 -0500

    Updates

---
+++ precache-manifest.28f0408a1b4da649c9083b87a6929553.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ceb1806562d0bfb9ab52c000477c2f31",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b8a0859f40f3ad3d49d8",
+    "url": "./static/css/main.9a3a7bd2.chunk.css"
+  },
+  {
+    "revision": "5274ae85dd08ed0588df",
+    "url": "./static/js/2.9db0954e.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "./static/js/2.9db0954e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8a0859f40f3ad3d49d8",
+    "url": "./static/js/main.bbb71b27.chunk.js"
+  },
+  {
+    "revision": "6461c24bbd3249b0c4f8",
+    "url": "./static/js/runtime-main.09d95451.js"
+  }
+]);
